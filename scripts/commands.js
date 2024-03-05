c3ff979d1aa542bfe528e434bc2ee9506bbeb218
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2024-03-05 10:41:12 -0700

    use commands module

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,5 +1,25 @@
 let commands = {
-    "about": "About Me",
-}
+    "about": `Hello,<br />
+    I am Mathew Sjoquist. I enjoy reading, playing games, and solving puzzles.
+    `,
+    "skills": `{<br />
+        "languages": ["Java", "JavaScript", "Python"],<br />
+        "frameworks": ["Spring", "Express"],<br />
+        "architectures": ["Microservices", "RESTful"],<br />
+        "tools": ["Git", "Docker"],<br />
+        "databases": ["SQL", "MongoDB", "Neo4j"],<br />
+    }`,
+    "contact": `{<br />
+        "email": "mathew.sjoquist@gmail.com",<br />
+        "indeed": "https://profile.indeed.com/p/mathews-kyl8pg9",<br />
+        "linkedin": "https://www.linkedin.com/in/mathew-sjoquist-8558bb299",<br />
+    }`,
+    "help": `{<br />
+        "about": "About me",<br />
+        "skills": "Technical skills",<br />
+        "contact": "Contact information",<br />
+        "help": "This description",<br />
+    }`,
+};
 
 export default commands;
