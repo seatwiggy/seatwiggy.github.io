2adb0e16d2dcac69cfb176ca74d0554f62c38c5f
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2024-03-06 19:53:42 -0700

    improved help

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,11 @@ function runCommand(event) {
     input = input.toLowerCase().split(/\s+/);
 
     elements.output.innerHTML += `<p>${input}</p>`;
-    elements.output.innerHTML += `<p>${commands[input[0]]}</p>`;
+    if (commands[input[0]] === undefined) {
+        elements.output.innerHTML += `<p>Command "${input[0]}" not found. Type "help" for a list of commands.</p>`;
+    } else {
+        elements.output.innerHTML += `<p>${commands[input[0]]}</p>`;
+    }
 }
 
 const commands = {
@@ -33,10 +37,8 @@ const commands = {
         "indeed": "https://profile.indeed.com/p/mathews-kyl8pg9",<br />
         "linkedin": "https://www.linkedin.com/in/mathew-sjoquist-8558bb299",<br />
     }`,
-    "help": `{<br />
-        "about": "About me",<br />
-        "skills": "Technical skills",<br />
-        "contact": "Contact information",<br />
-        "help": "This description",<br />
-    }`,
+    "help": `about &nbsp; About me<br />
+        skills &nbsp; Technical skills<br />
+        contact &nbsp; Contact information<br />
+        help &nbsp; This description`,
 };
