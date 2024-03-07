581dae2c9abe86bbf199b1b8d35a338c543890c7
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2024-03-06 19:33:13 -0700

    remove module

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-import commands from "./commands.js";
-
 let elements = {
     "output": document.getElementById("output"),
     "prompt": document.getElementById("prompt"),
@@ -17,3 +15,28 @@ function runCommand(event) {
     elements.output.innerHTML += `<p>${input}</p>`;
     elements.output.innerHTML += `<p>${commands[input[0]]}</p>`;
 }
+
+const commands = {
+    "about": `Hello,<br />
+    I am Mathew Sjoquist. I enjoy reading, playing games, and solving puzzles.<br />
+    I earned my degree in Software Engineering from Neumont College of Computer Science in Salt Lake City, Utah.
+    `,
+    "skills": `{<br />
+        "languages": ["Java", "JavaScript", "Python"],<br />
+        "frameworks": ["Spring", "Express"],<br />
+        "architectures": ["Microservices", "RESTful"],<br />
+        "tools": ["Git", "Docker"],<br />
+        "databases": ["SQL", "MongoDB", "Neo4j"],<br />
+    }`,
+    "contact": `{<br />
+        "email": "mathew.sjoquist@gmail.com",<br />
+        "indeed": "https://profile.indeed.com/p/mathews-kyl8pg9",<br />
+        "linkedin": "https://www.linkedin.com/in/mathew-sjoquist-8558bb299",<br />
+    }`,
+    "help": `{<br />
+        "about": "About me",<br />
+        "skills": "Technical skills",<br />
+        "contact": "Contact information",<br />
+        "help": "This description",<br />
+    }`,
+};
